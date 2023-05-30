81ae8bdd2ab520b889602b3f46f1485f7af11dc3
Krishna R Acharya <krishnaacharya.official@gmail.com>
2023-05-30 15:03:32 +0530

    basic vite setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import "./styles/globals.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+const activeChain = "ethereum";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+	<React.StrictMode>
+		<ThirdwebProvider activeChain={Sepolia}>
+			<App />
+		</ThirdwebProvider>
+	</React.StrictMode>
+);
